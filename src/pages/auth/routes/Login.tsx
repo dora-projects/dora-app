41822f59efb7f0268bbc09557b1532a6b2013a39
nan <msg@nancode.cn>
2021-11-19 21:10:17 +0800

    feat: 增加演示站点警告

--- src/pages/auth/routes/Login.tsx
+++ src/pages/auth/routes/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { message, Tabs, Button } from "antd";
+import { Alert, Tabs, Button } from "antd";
 import { useNavigate } from "react-router-dom";
-import { LoginForm, ProFormText, ProFormCaptcha, ProFormCheckbox } from "@ant-design/pro-form";
+import { LoginForm, ProFormCheckbox } from "@ant-design/pro-form";
 import Logo from "@/assets/logo.png";
 import { loginWithEmailAndPassword } from "@/services/auth";
 import storage from "@/utils/storage";
@@ -31,56 +31,61 @@ export const Login = () => {
     },
   });
 
+  const isDemoSite = window.location.host === "dora.nancode.cn";
+
   return (
-    <div style={{ backgroundColor: "white", paddingTop: "30px" }}>
-      <LoginForm
-        logo={Logo}
-        title="Dora"
-        subTitle="前端监控系统"
-        initialValues={{
-          autoLogin: true,
-        }}
-        onValuesChange={(v, all) => {
-          if (all.autoLogin) {
-          }
-        }}
-        onFinish={async (values) => {
-          const { email, password } = values;
-          login({ email, password });
-        }}
-      >
-        <Tabs activeKey={loginType} onChange={(activeKey) => setLoginType(activeKey as LoginType)}>
-          <Tabs.TabPane key={"email"} tab={"密码登录"} />
-          <Tabs.TabPane key={"code"} tab={"验证码登录"} disabled />
-        </Tabs>
-
-        {loginType === "email" && <AccountPassword />}
-        {loginType === "code" && <EmailCode />}
-
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            marginBottom: 20,
+    <>
+      {isDemoSite && <Alert message="该站点为演示环境，项目数据可能随时会被清空!" type="warning" closable banner />}
+      <div style={{ backgroundColor: "white", paddingTop: "30px" }}>
+        <LoginForm
+          logo={Logo}
+          title="Dora"
+          subTitle="前端监控系统"
+          initialValues={{
+            autoLogin: true,
+          }}
+          onValuesChange={(v, all) => {
+            if (all.autoLogin) {
+            }
+          }}
+          onFinish={async (values) => {
+            const { email, password } = values;
+            login({ email, password });
           }}
         >
-          <ProFormCheckbox noStyle name="autoLogin">
-            记住我
-          </ProFormCheckbox>
-          <span>
-            新用户？
-            <Button
-              type={"link"}
-              style={{ padding: 0 }}
-              onClick={() => {
-                navigate("/auth/register");
-              }}
-            >
-              去注册
-            </Button>
-          </span>
-        </div>
-      </LoginForm>
-    </div>
+          <Tabs activeKey={loginType} onChange={(activeKey) => setLoginType(activeKey as LoginType)}>
+            <Tabs.TabPane key={"email"} tab={"密码登录"} />
+            <Tabs.TabPane key={"code"} tab={"验证码登录"} disabled />
+          </Tabs>
+
+          {loginType === "email" && <AccountPassword />}
+          {loginType === "code" && <EmailCode />}
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: 20,
+            }}
+          >
+            <ProFormCheckbox noStyle name="autoLogin">
+              记住我
+            </ProFormCheckbox>
+            <span>
+              新用户？
+              <Button
+                type={"link"}
+                style={{ padding: 0 }}
+                onClick={() => {
+                  navigate("/auth/register");
+                }}
+              >
+                去注册
+              </Button>
+            </span>
+          </div>
+        </LoginForm>
+      </div>
+    </>
   );
 };
